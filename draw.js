fa5276f3e86979a9fe483598a36e519fd2ca2c9c
laelae <lailanofal@gmail.com>
2017-04-19 17:51:29 -0700

    timeline and fix stuff

--- draw.js
+++ draw.js
@@ -3,12 +3,21 @@ var canvas = document.getElementById("drawydrawdraw");
     canvas.height = window.innerHeight; //document.height is obsolete
 var context = canvas.getContext("2d");
 var drawing = false;
+//var hue;
 
+function rainbowRainbow() {
+	var space = document.body.clientWidth / 360;    
+for (var i = 0; i < 360; i = i + 1){
+    context.fillStyle = "hsl(" + i + ", 100%, 50%)";
+    context.fillRect(i * space,window.innerHeight-50,space,50);
+}
+}
 rainbowRainbow()
 
 $(canvas).mousedown(function(e){ 
     drawing = true;
     if(e.pageY>=window.innerHeight-50){
+		var space = document.body.clientWidth / 360; 
 		var hue = Math.floor(e.pageX / space);
         context.strokeStyle = "hsl(" + hue + ", 100%, 50%)";
 		context.lineWidth=10;
@@ -22,6 +31,9 @@ $(canvas).mousemove(function(e){
     if (drawing && e.pageY<window.innerHeight-50){
         context.lineTo(e.pageX, e.pageY);
         context.stroke();
+//		console.log(hue);
+//		context.strokeStyle = "hsl(" + hue + ", 100%, 50%)";
+
     } 
 
 });
@@ -36,10 +48,4 @@ function leonard() {
 	context.clearRect(0,0,canvas.width,canvas.height-50);
 	rainbowRainbow()
 }
-function rainbowRainbow() {
-	var space = document.body.clientWidth / 360;    
-for (var i = 0; i < 360; i = i + 1){
-    context.fillStyle = "hsl(" + i + ", 100%, 50%)";
-    context.fillRect(i * space,window.innerHeight-50,space,50);
-}
-}
+
